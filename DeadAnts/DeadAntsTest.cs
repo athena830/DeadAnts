8e934416632105bad518ae727afceba32e588b44
athena830 <athena830@gmail.com>
2017-09-08 11:06:02 +0800

    First Commit

---
+++ DeadAnts/DeadAntsTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DeadAnts
+{
+    [TestClass]
+    public class DeadAntsTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
